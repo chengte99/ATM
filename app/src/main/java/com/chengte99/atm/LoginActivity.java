a8bdb4ce03e60458f4053dcd77086253fbcbbe60
chengte99 <chengte99@gmail.com>
2020-05-25 10:46:35 +0800

    1. firebase realtime database 2. sharedRreferences

--- app/src/main/java/com/chengte99/atm/LoginActivity.java
+++ app/src/main/java/com/chengte99/atm/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.chengte99.atm;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.chengte99.atm.databinding.ActivityLoginBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = LoginActivity.class.getSimpleName();
     private ActivityLoginBinding binding;
 
     @Override
@@ -16,14 +24,49 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        String acc_storage = getSharedPreferences("atm", MODE_PRIVATE).getString("ACC", "");
+        Log.d(TAG, "onCreate: " + acc_storage);
+        binding.account.setText(acc_storage);
     }
 
     public void login(View view) {
-        String acc = binding.account.getText().toString();
-        String pw = binding.password.getText().toString();
-        if ("jack".equals(acc) && "1234".equals(pw)) {
-            finish();
-        }
+        final String account = binding.account.getText().toString();
+        final String password = binding.password.getText().toString();
+
+        FirebaseDatabase.getInstance().getReference("users")
+                .child(account).child("password")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String pw = (String) dataSnapshot.getValue();
+                        if (pw.equals(password)) {
+
+                            getSharedPreferences("atm", MODE_PRIVATE).edit()
+                                    .putString("ACC", account)
+                                    .apply();
+
+                            setResult(RESULT_OK);
+                            finish();
+                        }else {
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("Error")
+                                    .setMessage("Login Failed")
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+//        if ("jack".equals(account) && "1234".equals(password)) {
+//            setResult(RESULT_OK);
+//            finish();
+//        }
     }
 
     public void quit(View view) {
--- app/src/main/java/com/chengte99/atm/MainActivity.java
+++ app/src/main/java/com/chengte99/atm/MainActivity.java
@@ -1,24 +1,34 @@
 package com.chengte99.atm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static boolean logon = false;
+    private static final int REQUEST_CODE = 100;
+    private boolean logon = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (!logon) {
             Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+//            startActivity(intent);
+            startActivityForResult(intent, REQUEST_CODE);
         }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                finish();
+            }
+        }
+    }
 }
