4376aec28e9d33859781b7ff296817636eb39543
chengte99 <chengte99@gmail.com>
2020-05-25 15:47:53 +0800

    RecycleView

--- app/src/main/java/com/chengte99/atm/MainActivity.java
+++ app/src/main/java/com/chengte99/atm/MainActivity.java
@@ -2,13 +2,18 @@ package com.chengte99.atm;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 100;
+    private static final String TAG = MainActivity.class.getSimpleName();
     private boolean logon = false;
+    String[] functions = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //Adapter
+        FunctionAdapter adapter = new FunctionAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+//        Log.d(TAG, "onCreate: " + functions[0]);
     }
 
     @Override
