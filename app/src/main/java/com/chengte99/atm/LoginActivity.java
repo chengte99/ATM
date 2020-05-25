84c1f47b082e7f25d2168da6d13f3b78215f9de7
chengte99 <chengte99@gmail.com>
2020-05-25 17:46:45 +0800

    危險權限，相機

--- app/src/main/java/com/chengte99/atm/LoginActivity.java
+++ app/src/main/java/com/chengte99/atm/LoginActivity.java
@@ -3,8 +3,14 @@ package com.chengte99.atm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -19,6 +25,7 @@ import com.google.firebase.database.ValueEventListener;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final int REQUEST_CAM = 5;
     private ActivityLoginBinding binding;
 
     @Override
@@ -26,6 +33,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            toCapture();
+        }else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAM);
+        }
 
         binding.cbRemUserid.setChecked(
                 getSharedPreferences("atm", MODE_PRIVATE).getBoolean("REMEMBER_USERID", false)
@@ -43,6 +57,21 @@ public class LoginActivity extends AppCompatActivity {
         String acc_storage = getSharedPreferences("atm", MODE_PRIVATE).getString("ACC", "");
         Log.d(TAG, "onCreate: " + acc_storage);
         binding.account.setText(acc_storage);
+    }
+
+    private void toCapture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAM) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                toCapture();
+            }
+        }
     }
 
     public void login(View view) {
